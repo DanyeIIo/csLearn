7bc1abedc68aecd0942b3b8ef8a91852acc7d143
Danyello <ulkiora.panda@gmail.com>
2021-04-29 03:31:30 +0600

    added a server for udp

---
+++ UDP/UDP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8087;
+
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+
+
+
+            while (true)
+            {
+                var buffer = new byte[256];
+                var size = 0;
+                var data = new StringBuilder();
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0); //here will be save client adress
+                
+                do
+                {
+                    size = udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
+                    data.Append(Encoding.UTF8.GetString(buffer));
+                }
+                while (udpSocket.Available > 0);
+
+                udpSocket.SendTo(Encoding.UTF8.GetBytes("Message has been catched"), senderEndPoint);
+
+                Console.WriteLine(data);
+            }
+        }
+        // todo: socket close
+    }
+}
